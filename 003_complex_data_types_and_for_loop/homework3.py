7b10c79d0c3ab9f269e1f1101ed6b9b79d8d44f3
Roman Kutselepa <roman.kutselepa@gmail.com>
2025-02-07 14:52:55 +0200

    For cycle, if elif else

--- 003_complex_data_types_and_for_loop/homework3.py
+++ 003_complex_data_types_and_for_loop/homework3.py
@@ -1,28 +1,48 @@
 # Print to console what is different in each set compared to another
 set_a = {1, 2, 3, 4}
 set_b = {3, 4, 5, 6}
+print(set_a.difference(set_b))
+print(set_b.difference(set_a))
 
 
 # Create a string from a list and save it to variable
 # Make each name on a new line.
 names = ['Jack', 'Mary', 'Samantha', 'George', 'Simon', 'John']
-
+new_line = '\n'.join(names)
+# print(new_line)
+# nl = ''
+# for name in names:
+#     nl += name + "\n"
+# print(nl)
 
 # print sum of all numbers in a list
 # print sum of all unique numbers in a list
 numbers = [2, 53, 12, 87, 65, 32, 12, 2, 65, 32]
+print(sum(numbers))
+print(sum(set(numbers)))
 
 
 # create a new list from studentsA and studentsB
 # make sure there is no duplicates in a new lists
 studentsA = ['Jack', 'Bob', 'Mary']
 studentsB = ['Bob', 'Sarah', 'Simon']
-
+new_lst = list(set(studentsA + studentsB))
+print(new_lst)
 
 # What elements are common for both tuples?
 numbersA = (23, 52, 12, 75, 42)
 numbersB = (75, 22, 42, 94, 70)
+# common = set(numbersA).intersection(set(numbersB))
+# print('Common numbers are:', end=" ")
+# for num in common:
+#     print(num, end=", ")
 
 
 # add 5 to the tuple on a right position
 a = (1, 2, 3, 4, 6, 7, 8)
+a = list(a)
+# a.insert(4, 5)
+a.append(5)
+a.sort()
+a = tuple(a)
+print(a)
--- 004_dictionaries_for_cycle/homework.py
+++ 004_dictionaries_for_cycle/homework.py
@@ -1,23 +1,51 @@
 # Add a list of elements to a set
 sample_set = {"Yellow", "Orange", "Black"}
 sample_list = ["Blue", "Green", "Red"]
-
+# sample_set.update(sample_list)
+# sample_set |= set(sample_list)
+# print(sample_set)
 
 # Return a new set of identical items from two sets
 set1 = {10, 20, 30, 40, 50}
 set2 = {30, 40, 50, 60, 70}
+# set3 = set1.intersection(set2)
+# set3 = set()
+# set3 &= set2
+# print(set1)
 
 
 # Create two empty lists (long_names, short_names)
 # Iterate through names list and add names that are longer than 5 characters
 # to long_names, and others to short names
 names = ['Sarah', 'Jessica', 'Anthony', 'Jack', 'Simon', 'Arthur', 'Maria', 'Samantha']
+# long_names, short_names = [], []
+# for name in names:
+#     if len(name) > 5:
+#         long_names.append(name)
+#     else:
+#         short_names.append(name)
+# print(long_names)
+# print(short_names)
 
 
 # Given a list where each element is a year. Determine whether the given year is a leap year. If the year is a leap year,
 # print YES, otherwise print NO. According to the Gregorian calendar, a year is a leap year if its number is a multiple of 4,
 # but not a multiple of 100 OR if it is a multiple of 400.
+
+
 years_list = [2012, 2011, 1492, 1861, 1600, 1700, 1800, 1900, 2000]
+for y in years_list:
+    if y % 4 == 0 and y % 100 != 0 or y % 400 == 0:
+        print(y, 'YES')
+    else:
+        print(y, 'NO')
 
 
 # Write a program that prompts the user for a string and checks if the string contains only unique characters.
+user_prompt = input('Enter something: ')
+if len(user_prompt) == len(set(user_prompt)):
+    print('Unique')
+else:
+    print('Not unique')
+
+print(set('Hello world!'))
